4d6a9c5825caf45254c39554d0b94d3a510b2408
bhimrazy <bhimrajyadav977@gmail.com>
2020-09-29 20:34:21 +0545

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./components/Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
+    }
+  }, []);
+  return <div className="app">{token ? <h1>Logged In</h1> : <Login />}</div>;
 }
 
 export default App;
