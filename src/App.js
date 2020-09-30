c80b5c4de306b4c8b4c176e24815ea1dc0e0e896
bhimrazy <bhimrajyadav977@gmail.com>
2020-09-30 17:57:55 +0545

    player and sidebar file added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { useDataLayerValue } from "./context api/DataLayer";
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
   const [{}, dispatch] = useDataLayerValue();
 
   useEffect(() => {
@@ -16,15 +15,25 @@ function App() {
     window.location.hash = "";
     const _token = hash.access_token;
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
       spotify.setAccessToken(_token);
 
       spotify.getMe().then((user) => {
-        console.log(user);
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
       });
     }
   }, []);
-  return <div className="app">{token ? <Player /> : <Login />}</div>;
+  return (
+    <div className="app">
+      {token ? <Player spotify={spotify} /> : <Login />}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Player.css";
+import Sidebar from "./Sidebar";
 
-function Player() {
-  return <div className="player"></div>;
+function Player({ spotify }) {
+  return (
+    <div className="player">
+      <div className="player__body">
+        <Sidebar />
+      </div>
+    </div>
+  );
 }
 
 export default Player;
