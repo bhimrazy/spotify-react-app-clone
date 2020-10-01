175fc7bd9a8fec5a44207f0919f5652c595e259f
bhimrazy <bhimrajyadav977@gmail.com>
2020-10-01 18:45:38 +0545

    body icons part done

--- src/App.js
+++ src/App.js
@@ -34,6 +34,12 @@ function App() {
           playlists: playlists,
         });
       });
+      spotify.getPlaylist("37i9dQZF1E35jI8gotbcvR").then((response) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        });
+      });
     }
   }, []);
   return (
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,33 @@
 import React from "react";
+import { useDataLayerValue } from "../context-api/DataLayer";
 import "./Body.css";
 import Header from "./Header";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+
 function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = useDataLayerValue();
   return (
     <div className="body">
       <Header spotify={spotify} />
       <div className="body__info">
-        <img
-          src="https://dailymix-images.scdn.co/v2/img/d9cf81f26581c55c3ef9aba1acc1332ee915c30a/1/en/large"
-          alt=""
-        />
+        <img src={discover_weekly?.images[0]?.url} alt="" />
         <div className="body__infoText">
           <strong>PLAYLIST</strong>
           <h2>Discover Weekly</h2>
-          <p>description</p>
+          <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle" />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
+        </div>
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow track={item.track} />
+        ))}
       </div>
     </div>
   );
